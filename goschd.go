2f5057fe4b3913a9d37608f78a9ca2087a4810d5
ganony <gillescpp@gmail.com>
2019-09-29 11:22:16 +0200

    wip

---
+++ goschd.go
@@ -0,0 +1,21 @@
+package goschd
+
+import (
+	"fmt"
+)
+
+// Event is a schedule définition
+type Event struct {
+
+}
+
+// InitFromString todotdo
+func (c* Event)InitFromString(str string) {
+
+}
+
+
+// Scheduler instance
+type Scheduler struct {
+
+}
